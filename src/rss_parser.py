420edf5c87f79717d27ec533c2ec289a6771ac50
Tomi Gonzalia <tomigonzalia@gmail.com>
2023-08-03 00:48:18 -0300

    fix: bug in pubDate tag

--- src/rss_parser.py
+++ src/rss_parser.py
@@ -4,10 +4,9 @@ import xml.etree.ElementTree as ET
 from datetime import datetime
 
 
-
 class RssParser:
 
-    def parse_for_rss(response_xml:str):
+    def parse_for_rss(response_xml: str):
 
         f = Feed('Parser')
         root = ET.fromstring(response_xml)
@@ -15,9 +14,9 @@ class RssParser:
         for item in root.findall('.//item'):
             f.add_article(Article(
                 title=item.findtext('title'),
-                text = item.findtext('description'),
+                text=item.findtext('description'),
                 link=item.findtext('link'),
-                publication_date=item.findtext('pubdate')
+                publication_date=item.findtext('pubDate')
             ))
 
         return f.get_article_list()
